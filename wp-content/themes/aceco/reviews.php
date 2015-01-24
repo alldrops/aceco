7ae2505d6dd9d7081ae6de1fec61e6b3ac6c055d
Andre Lopes <andre.22.lopes@gmail.com>
2015-01-24 03:35:58 -0800

    Reviews

--- wp-content/themes/aceco/reviews.php
+++ wp-content/themes/aceco/reviews.php
@@ -2,73 +2,34 @@
 
 <?php include("header.php"); ?>
 
+<?php while ( have_posts() ) : the_post(); ?>
 <div class="main-content">
 	<div class="wrapper">
 		<div class="content-section">
-		
-			<?php while ( have_posts() ) : the_post(); ?>
-				<div class="content-block">
-					<div class="content-wrapper">
-						<h2><?php the_field('top_title'); ?></h2>
-						<?php 
-					    $top_desc = get_field('top_description', $post->ID);
-					    if( !empty($top_desc) ): ?>
-						        
-							<div class="text">
-								<p>
-									<?php echo $top_desc; ?>
-								</p>
-							</div>
-						<?php endif; ?>
-					</div>
+			<div class="content-block">
+				<div class="content-wrapper">
+					<h2><?php the_field('title'); ?></h2>
+					<?php 
+				    $top_desc = get_field('top_description', $post->ID);
+				    if( !empty($top_desc) ): ?>
+					        
+						<div class="text">
+							<p>
+								<?php echo $top_desc; ?>
+							</p>
+						</div>
+					<?php endif; ?>
+					<a href="write-your-review/" class="write-review">Write Your Review</a>
 				</div>
-			<?php endwhile; ?>
-
+			</div>
 		</div>
 		<div class="content-section">
-			<div class="content-block blog-posts-listing">
-				
-				<?php 
-				// the query
-				$args = array(
-				    'post_type'             => 'post',
-				    'posts_per_page'        => -1
-				);
-
-				$the_query = new WP_Query( $args ); ?>
-
-				<?php if ( $the_query->have_posts() ) : ?>
-
-					<!-- pagination here -->
-
-					<!-- the loop -->
-					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-						<div class="post">
-							<h3><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h3>
-							<div class="post-info-title">
-								<p><?php echo get_the_date('d.m.Y'); ?></p>
-								<p class="time"><?php echo get_the_time(); ?></p>
-								<div class="excerpt">
-									<a href="<?php echo get_permalink(); ?>">
-										<?php echo apply_filters('the_content', substr(get_the_content(), 0, 100) ); ?>
-									</a>
-								</div>
-								<a class="read-more" href="<?php echo get_permalink(); ?>">Read More</a>
-							</div>
-						</div>
-					<?php endwhile; ?>
-					<!-- end of the loop -->
-
-					<!-- pagination here -->
-
-					<?php wp_reset_postdata(); ?>
-
-				<?php else : ?>
-					<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-				<?php endif; ?>
+			<div class="content-block reviews-listing">
+				<?php the_content(); ?>
 			</div>
 		</div>
 	</div>
 </div>
+<?php endwhile; ?>
 
 <?php include("footer.php"); ?>
